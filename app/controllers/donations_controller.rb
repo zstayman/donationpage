99c707b99f06b985411695362b725186dabb76e9
github_name <z.stayman@gmail.com>
2014-06-02 15:36:28 -0400

    error handling working

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,14 +2,17 @@ class DonationsController < ApplicationController
   def new
   end
   def create
-    binding.pry
     charge = Stripe::Charge.create(
       amount: (params[:amount].to_i * 100),
       currency: "usd",
       card: params[:stripeToken],
       description: "TEST Web Donation"
       )
+    redirect_to request.referer
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
     binding.pry
     redirect_to request.referer
+
   end
 end

ca01ae905bab6bd76460526dd7d1ce851039804e
github_name <z.stayman@gmail.com>
2014-06-02 20:34:46 -0400

    checkpoint

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -8,9 +8,36 @@ class DonationsController < ApplicationController
       card: params[:stripeToken],
       description: "TEST Web Donation",
       )
+    donation = {
+      external_id: charge["id"],
+      firstname: params[:first_name],
+      lastname: params[:last_name],
+      transaction_dt: Time.now.to_s,
+      amount: params[:amount].to_i,
+      cc_type_cd: cc_conversion(charge["card"]["type"]),
+      addr1: params[:address_1],
+      addr2: params[:address_2],
+      city: params[:city],
+      state: params[:state].downcase
+    }.to_json
+    binding.pry
+    HTTParty.post("", donation)
     redirect_to request.referer
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to request.referer
+  end
+
+  private
+
+  def cc_conversion(type)
+    cards = {
+      "Visa" => 'vs',
+      "MasterCard" => 'mc',
+      "American Express" => 'ax',
+      "Discover" => 'ds'
+    }
+
+    return cards[type]
   end
 end

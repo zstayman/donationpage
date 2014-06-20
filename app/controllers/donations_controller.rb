09f8a7106134d2b0a77d50f3164291990e440494
github_name <z.stayman@gmail.com>
2014-06-20 18:40:34 -0400

    BSD WORKING

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -8,20 +8,6 @@ class DonationsController < ApplicationController
       card: params[:stripeToken],
       description: "TEST Web Donation",
       )
-    @time = Time.now.to_i
-    donation = {
-      external_id: charge["id"],
-      firstname: params[:first_name],
-      lastname: params[:last_name],
-      transaction_dt: Time.now.to_s.split.slice(0..1).join(" "),
-      amount: params[:amount].to_i,
-      cc_type_cd: cc_conversion(charge["card"]["type"]),
-      addr1: params[:address_1],
-      addr2: params[:address_2],
-      city: params[:city],
-      state: params[:state].downcase
-    }
-    HTTParty.post("https://trujillo.cp.bsd.net/page/api/contribution/add_external_contribution?api_ver=2&api_id=Signup-Donation&api_ts=#{@time}&api_mac=#{hashify(donation)}", donation)
     redirect_to request.referer
   rescue Stripe::CardError => e
     flash[:error] = e.message
@@ -38,18 +24,7 @@ class DonationsController < ApplicationController
       "Discover" => 'ds'
     }
 
-    return cards[type]
+    cards[type]
   end
 
-  def hashify(donation)
-    signing_string = "Signup-Donation
-    #{@time}
-    /page/api/contribution/add_external_contribution
-    api_version=2&api_id"
-    donation.each do |key, value|
-      signing_string += "&#{key.to_s}=#{value}"
-    end
-    api_mac = OpenSSL::HMAC.hexdigest("sha1", BSD_SECRET, signing_string)
-    return api_mac
-  end
 end
--- config/initializers/bsd.rb
+++ config/initializers/bsd.rb
@@ -1,1 +1,1 @@
-BSD_SECRET = ENV['BSD_SECRET']
+$CONNECTION = BlueStateDigital::Connection.new(host: ENV['BSD_TRUJILLO_ROUTE'], api_id: ENV['BSD_API_ID'], api_secret: ENV['BSD_SECRET'])

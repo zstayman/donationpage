41c0fe1d8a87463b580bc323f2ff57ca65c23b4a
github_name <z.stayman@gmail.com>
2014-06-29 09:40:03 -0400

    next

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -9,14 +9,14 @@ class DonationsController < ApplicationController
       description: "TEST Web Donation",
       )
 
-    cont = BlueStateDigital::Contribution.new(connection: $CONNECTION,
-                                              external_id: charge["id"],
-                                              firstname: params[:first_name],
-                                              lastname: params[:last_name],
-                                              transaction_amt: params[:amount].to_i,
-                                              cc_type_cd: cc_conversion(charge["card"]["brand"]),
-                                              transaction_dt: DateTime.now.to_s
-                                              )
+    cont = BlueStateDigital::Contribution.new({connection: $CONNECTION,
+                                                external_id: charge["id"],
+                                                firstname: params[:first_name],
+                                                lastname: params[:last_name],
+                                                transaction_amt: params[:amount].to_i,
+                                                cc_type_cd: cc_conversion(charge["card"]["brand"]),
+                                                transaction_dt: DateTime.now.to_s
+                                              })
     cont.save
     redirect_to "http://www.kentrujillo.com"
   rescue Stripe::CardError => e
